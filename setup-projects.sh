8b08013c62d0d7ed97fa21143a25855086a96099
jmeyo <jc.meillaud@gmail.com>
2015-02-27 16:28:00 +0100

    setup script for nginx configuration files

---
+++ setup-projects.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+SM_CONF_DIR=/root/.symfony-manager/sm-config
+
+for file in `ls $SM_CONF_DIR`
+do
+    source $SM_CONF_DIR
+    cat /tmp/default-symfony-nginx.conf | sed "s/__project_name__/$application_projectname/g;s#__project_path__#$application_install_path#g;s/__project_hosts__/$application_host=/g"  > /etc/nginx/sites-available/project_$application_projectname.conf
+done
